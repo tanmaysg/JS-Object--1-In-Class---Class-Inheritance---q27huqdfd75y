5be71cc4ed5dd5f881cfd1d5245775be4ede04c6
tanmaysg <tanmaysg@gmail.com>
2021-12-02 11:53:00 +0530

    Update shape.js

--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,18 @@
 // declare class
+  class Shape {
+   
+    constructor(color) {
+        this.color = "RED";
+    }
 
+    drawShape() {
+        console.log(`${this.color} is colored`);
+
+    }
+    calculateArea() {
+        console.log(`${this.color}  area is to be calculated`)
+    }
+
+}
+module.exports = Shape
 // export class using module.exports
